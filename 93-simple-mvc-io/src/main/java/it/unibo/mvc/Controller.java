655cfbf758ad39ab8c7a021f90b3a72897a954a4
keliane2 <kelianetchoumkeu@gmail.com>
2023-11-20 22:48:37 +0100

    93- Controller ok

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,34 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private static File currentFile=new File(System.getProperty("user.home")+File.separator+"output.txt");
 
+    public void setCurrentFile(File f){
+        this.currentFile=f;
+    }
+
+    public File getCurrentFile(){
+        return this.currentFile;
+    }
+
+    public String getCurrentFilePath(){
+        return this.currentFile.getPath();
+    }
+
+    public void writeInCurrentFile(String s){
+        try {
+            PrintStream ps = new PrintStream(this.currentFile);
+            ps.print(s);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
