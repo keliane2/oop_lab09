721437e9c40a89bc98be8e3ad9e94c75cd9e129d
keliane2 <kelianetchoumkeu@gmail.com>
2023-11-20 22:50:30 +0100

    93-simple GUI ok

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,12 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +14,22 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("My first java graphical interface");
 
+    public SimpleGUI(){
+        JPanel canvas=new JPanel();
+        canvas.setLayout(new BorderLayout());
+        JTextArea textArea=new JTextArea("Write here");
+        canvas.add(textArea, BorderLayout.CENTER);
+        JButton save = new JButton("save");
+        canvas.add(save, BorderLayout.PAGE_END);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(canvas);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(final String... args){
+        new SimpleGUI();
+    }
 }
